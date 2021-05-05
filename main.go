388c301bf25487c92253d178d91b3644f624d5ed
mosn1bw <81889769+mosn1bw@users.noreply.github.com>
2021-05-05 16:36:48 +0430

    Update main.go

--- main.go
+++ main.go
@@ -2996,6 +2996,30 @@ func bible(text string,user_msgid string,reply_mode string) (string, string, str
 	}
 }
 
-func Contains(s, substr string) bool { 
-     return Index(s, substr) != -1
+func callbackHandler(w http.ResponseWriter, r *http.Request) {
+	events, err := bot.ParseRequest(r)
+
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
+		}
+		return
+	}
+
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				quota, err := bot.GetMessageQuota().Do()
+				if err != nil {
+					log.Println("Quota err:", err)
+				}
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK! remain message:"+strconv.FormatInt(quota.Value, 10))).Do(); err != nil {
+					log.Print(err)
+				}
+			}
+		}
+	}
 }
